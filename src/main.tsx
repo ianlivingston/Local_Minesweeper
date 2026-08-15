4755942ae064250f35260b38162bb3825c516779
Ian Livingston <ian.n.livingston@outlook.com>
2026-08-15 13:26:27 -0500

    fix: minesweeper html pointing to invalid uri

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import Login from "./Login.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
             <Route index element={<Game />}/>
             <Route path="login" element={<Login />}/>
